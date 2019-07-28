1edebbddf56399f1021998eed7dc995a6dbc07c2
ojcm <ojcmuir@gmail.com>
2019-07-28 11:04:41 +0100

    Restructure monzo packages

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
-package monzotools
+package monzoutils
 
 import (
+	"errors"
+	"fmt"
+
 	monzo "github.com/tjvr/go-monzo"
 )
 
@@ -25,4 +28,83 @@ func GetFirstAccountFromAccessToken(accessToken string) monzo.Account {
 // account for the given access token.
 func GetFirstAccountIDFromAccessToken(accessToken string) string {
 	return GetFirstAccountFromAccessToken(accessToken).ID
+}
+
+// GetClient returns a client for interracting with accounts associated
+// with the given access token.
+func GetClient(accessToken string) monzo.Client {
+	return monzo.Client{
+		BaseURL:     "https://api.monzo.com",
+		AccessToken: accessToken,
+	}
+}
+
+func ProcessDeposits(cl monzo.Client, deposits []*monzo.DepositRequest) error {
+	for _, deposit := range deposits {
+		_, err := cl.Deposit(deposit)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func GetActivePots(cl monzo.Client) ([]*monzo.Pot, error) {
+	allPots, err := cl.Pots()
+	if err != nil {
+		return nil, err
+	}
+	var activePots []*monzo.Pot
+	for _, pot := range allPots {
+		if !pot.Deleted {
+			activePots = append(activePots, pot)
+		}
+	}
+	if len(activePots) == 0 {
+		return nil, errors.New("No active pots found")
+	}
+	return activePots, nil
+}
+
+func GetAccountWithID(cl monzo.Client, accountID string) (*monzo.Account, error) {
+
+	accounts, err := cl.Accounts("uk_retail")
+	if err != nil {
+		return nil, err
+	}
+
+	var sourceAccount *monzo.Account
+	for _, account := range accounts {
+		if account.ID == accountID {
+			sourceAccount = account
+			break
+		}
+	}
+	if sourceAccount == nil {
+		return nil, errors.New("Account associated with trigger transaction not found")
+	}
+	return sourceAccount, nil
+}
+
+// FormatPenceToGbp takes an integer number of pence and returns a string
+// representing that value in GBP.
+func FormatPenceToGbp(pennies int64) string {
+	return fmt.Sprintf("£%v", float64(pennies)/100)
+}
+
+func main() {
+	accessToken := "secret"
+	cl := GetClient(accessToken)
+	accountID := GetFirstAccountIDFromAccessToken(accessToken)
+	transactions, err := cl.Transactions(accountID, false) // don't expandMerchant
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, transaction := range transactions {
+		if transaction.Description == "MONTHLY SALARY" {
+			fmt.Println(transaction)
+		}
+	}
+
 }
